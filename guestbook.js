44ede32340ae62270040a17a189bb3fa3942c81b
floroe1988 <68111417+floroe1988@users.noreply.github.com>
2022-02-26 14:07:03 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e987810c.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
